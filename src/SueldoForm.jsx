fe28dd74c5f50ec8d7249162bb689db9db76adbc
Julián Leiva <144271549+julianlux10@users.noreply.github.com>
2024-02-07 16:06:12 -0500

    Update SueldoForm.jsx

--- src/SueldoForm.jsx
+++ src/SueldoForm.jsx
@@ -17,23 +17,23 @@ function SueldoForm({ onSueldoCalculado }) {
 
   return (
     <form className='form' onSubmit={handleSubmit}>
-      {/* Etiqueta y campo de entrada para el nombre */}
+
       <label><b>Nombre:</b>
         
       </label>
       <input type="text" value={nombre} onChange={(e) => setNombre(e.target.value)} />
       <br /> <br />
-      {/* Etiqueta y campo de entrada para las horas de trabajo */}
+     
       <label><b>Horas de Trabajo:</b>
       </label>
       <input type="number" value={horasTrabajo} onChange={(e) => setHorasTrabajo(e.target.value)} />
       <br /> <br />
-      {/* Etiqueta y campo de entrada para el valor del pago por hora */}
+
       <label><b>Valor del Pago por Hora:</b>
       </label>
       <input type="number" value={valorPagoHora} onChange={(e) => setValorPagoHora(e.target.value)} />
       <br />
-      {/* Botón para enviar el formulario */}
+   
       <button type="submit">Calcular Sueldo</button>
     </form>
   );
